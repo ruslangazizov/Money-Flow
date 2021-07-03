39d93a8f0e9151ab8fc81ba369394dd1b8f9fe68
Ruslan <ruslangazizov36@gmail.com>
2021-07-03 17:07:22 +0300

    Initial Commit

---
+++ Money Flow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Flow
+//
+//  Created by Руслан on 03.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
