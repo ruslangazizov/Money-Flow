fdab671c37e211645efbf71e8413a5458352a04f
Ruslan <ruslangazizov36@gmail.com>
2021-07-06 15:18:06 +0300

    Change statisticsButton from image to View and set it as getReportButtonAction

--- Money Flow/ScreenTemplates/MaratTemplates/ProfileeViewController.swift
+++ Money Flow/ScreenTemplates/MaratTemplates/ProfileeViewController.swift
@@ -9,20 +9,25 @@ import UIKit
 
 class ProfileeViewController: UIViewController {
     @IBOutlet weak var mainTextLabel: UILabel!
-    @IBOutlet weak var statisticsButton: UIButton!
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var panelView: UIView!
     @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var behindButtonView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         panelView.layer.cornerRadius = 35
+        behindButtonView.layer.cornerRadius = 20
+        behindButtonView.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        behindButtonView.layer.borderWidth = 3
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func getReportButtonAction(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
