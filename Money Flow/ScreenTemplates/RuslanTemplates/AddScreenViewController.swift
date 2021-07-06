d5fbe221d1dec46abeaecc584e3955617d97d98f
Ruslan <ruslangazizov36@gmail.com>
2021-07-06 12:20:20 +0300

    Adjust MaratTemplates & remove unused assets & add RuslanTemplates

---
+++ Money Flow/ScreenTemplates/RuslanTemplates/AddScreenViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddScreenViewController.swift
+//  
+//
+//  Created by Руслан on 05.07.2021.
+//
+
+import UIKit
+
+class AddScreenViewController: UIViewController {
+    
+    @IBOutlet weak var screenNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var dataTextField: UITextField!
+    @IBOutlet weak var costTextField: UITextField!
+    @IBOutlet weak var choiceTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func dropdownButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonAction(_ sender: Any) {
+    }
+}
