f61b6e6a09cf1fb587f8423c3d16f7c01a10ced7
Ruslan <ruslangazizov36@gmail.com>
2021-07-07 21:55:54 +0300

    Remove DataBase usage example

--- Money Flow/Expenses/ExpensesViewController.swift
+++ Money Flow/Expenses/ExpensesViewController.swift
@@ -11,16 +11,6 @@ class ExpensesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        DataBase.addExpense(ExpenseInfo(categoryName: "Транспорт", name: "шина", date: "07.07.2021", icon: "⚙️", cost: 500))
-        DataBase.addExpense(ExpenseInfo(categoryName: "Транспорт", name: "сервис", date: "01.06.2021", icon: "⚙️", cost: 1000))
-        DataBase.addExpense(ExpenseInfo(categoryName: "Шоппинг", name: "футболка", date: "07.07.2021", icon: "👕", cost: 50))
-        DataBase.addIncome(IncomeInfo(source: "Работа", date: "07.07.2021", icon: "😀", worth: 2000))
-        print(DataBase.getTotalIncome())
-        print(DataBase.getTotalExpenses())
-        print(DataBase.getMonthExpensesIn(month: "07"))
-        print(DataBase.getTotalExpensesIn(category: "Транспорт"))
-        print(DataBase.getMonthExpensesInCategory("Транспорт", month: "07"))
 
     }
     
