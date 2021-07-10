01045a795cc14125f00571775d8e674b100f64a5
Ruslan <ruslangazizov36@gmail.com>
2021-07-10 17:57:30 +0300

    Add settings first screen

---
+++ Money Flow/Settings/LimitsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LimitsViewController.swift
+//  Money Flow
+//
+//  Created by Руслан on 10.07.2021.
+//
+
+import UIKit
+
+class LimitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Money Flow/Settings/SettingsViewController.swift
+++ Money Flow/Settings/SettingsViewController.swift
@@ -8,9 +8,43 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBOutlet weak var gearImage: UIImageView!
+    
+    @IBOutlet weak var limitbutton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        limitbutton.layer.cornerRadius = 20
+        resetButton.layer.cornerRadius = 20
+    }
+
+    @IBAction func goToLimits(_ sender: Any) {
+        let newVC = storyboard?.instantiateViewController(identifier: "LimitsViewController") as? LimitsViewController ?? LimitsViewController()
+        navigationController?.pushViewController(newVC, animated: true)
+    }
+    
+    
+    @IBAction func deleteAll(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "Вы уверены?", preferredStyle: .alert)
+
+        let cancelAction = UIAlertAction(title: "Нет", style: .cancel) { _ in
+
+        }
+
+        let acceptAction = UIAlertAction(title: "Да", style: .default) { _ in
+            DataBase.deleteAllData()
+        }
+
+        alert.addAction(cancelAction)
+        alert.addAction(acceptAction)
+
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
