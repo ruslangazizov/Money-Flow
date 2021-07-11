324bd649698c5d4eb58e9019d25ff2b63743fe49
Ruslan <ruslangazizov36@gmail.com>
2021-07-11 13:57:00 +0300

    Add changes in ExpensesViewController due to the merge conflicts

--- Money Flow/Expenses/ExpensesViewController.swift
+++ Money Flow/Expenses/ExpensesViewController.swift
@@ -11,7 +11,53 @@ class ExpensesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        categoriesCollectionView.register(UINib(nibName: "CategoryCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CategoryCollectionViewCell")
+        categoriesCollectionView.dataSource = self
+        categoriesCollectionView.delegate = self
+    }
+    
+}
 
+extension ExpensesViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCollectionViewCell", for: indexPath) as? CategoryCollectionViewCell ?? CategoryCollectionViewCell()
+        cell.setupCell(categories[indexPath.item])
+        cell.delegate = self
+        return cell
+    }
+}
+
+extension ExpensesViewController: UICollectionViewDelegate {
+    
+}
+
+extension ExpensesViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = leftAndRigthMargin ?? 25
+        let paddingWidth = margin * itemsPerRowCount + widthBetweenItemsInLine * (itemsPerRowCount - 1)
+        let totalAvailableWidth = collectionView.frame.width - paddingWidth
+        let widthForCell = totalAvailableWidth / itemsPerRowCount
+        return CGSize(width: widthForCell, height: widthForCell)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let margin: CGFloat = leftAndRigthMargin ?? 25
+        return UIEdgeInsets(top: 0, left: margin, bottom: margin, right: margin)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        widthBetweenItemsInLine
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        20
     }
     
 }
